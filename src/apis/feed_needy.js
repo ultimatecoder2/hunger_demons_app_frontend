6df4ba69a06002a023a5ffe7f7f0479a452292b5
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-12-23 19:45:38 +0530

    updated npm browser versions

--- src/apis/feed_needy.js
+++ src/apis/feed_needy.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 const baseURL = "https://hunger-demons-backend.herokuapp.com/"
-const devURL = "http://localhost:3001"
 const record =  axios.create({
     baseURL:baseURL
 })
